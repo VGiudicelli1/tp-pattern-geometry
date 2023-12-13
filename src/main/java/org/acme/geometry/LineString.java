84fc5a0f703a83db5be988d7b18b0e0fcdc21742
VGiudicelli <vincent.giudicelli@ensg.eu>
2023-12-13 16:57:55 +0100

    add empty class Point LineString

---
+++ src/main/java/org/acme/geometry/LineString.java
@@ -0,0 +1,30 @@
+package org.acme.geometry;
+
+import java.util.List;
+
+public class LineString implements Geometry {
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public LineString() {
+		// TODO
+	}
+	
+	public LineString(List<Point> l) {
+		// TODO
+	}
+	
+	public int getNumPoints() {
+		// TODO
+		return 0;
+	}
+	
+	public Point getPointN(int n) {
+		// TODO
+		return null;
+	}
+}
---
+++ src/main/java/org/acme/geometry/Point.java
@@ -0,0 +1,25 @@
+package org.acme.geometry;
+
+public class Point implements Geometry {
+	
+	private Coordinate c;
+	
+	public Point() {
+		// TODO
+	}
+	
+	public Point(Coordinate c) {
+		// TODO
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Coordinate getCoordinate() {
+		return this.c;
+	}
+
+}
