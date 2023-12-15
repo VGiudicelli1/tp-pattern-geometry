61760e7e1dd10d06cecfa0597fcc6deead2183d0
VGiudicelli <vincent.giudicelli@ensg.eu>
2023-12-15 09:27:11 +0100

    add tests for lineString and Point

---
+++ src/test/java/org/acme/geometry/GeometryTest.java
@@ -0,0 +1,36 @@
+package org.acme.geometry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GeometryTest {
+
+	public static final double EPSILON = 1.0e-15;
+
+	@Test
+	public void testLineString() {
+		LineString l = new LineString();
+		Assert.assertEquals("LineString", l.getType());
+		Assert.assertEquals(l.getNumPoints(), 0);
+		
+		List<Point> lPts = new ArrayList<Point>();
+		lPts.add(new Point(new Coordinate(1.4, 5.7)));
+		lPts.add(new Point(new Coordinate(7.2, 4.8)));
+		LineString l2 = new LineString(lPts);
+		
+		Assert.assertEquals(l2.getNumPoints(), 2);
+		Assert.assertEquals(l2.getPointN(1).getCoordinate().getX(), 7.2, EPSILON);
+	}
+
+	@Test
+	public void testPoint() {
+		Point p = new Point();
+		Assert.assertEquals("Point", p.getType());
+		
+		Point p2 = new Point(new Coordinate(1.7, 5.9));
+		Assert.assertEquals(p2.getCoordinate().getX(), 1.7, EPSILON);
+	}
+}
